21df65d3b808d58965fb5985fe524ec7d0ca6e46
Henry <hpc3@njit.edu>
2020-03-13 16:18:15 -0400

    Integer Addition and BasicCalculatorTests

--- BasicCalculator/basicCalculator.cs
+++ BasicCalculator/basicCalculator.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MathOperations;
+using Addition;
 
 namespace BasicCalculator
 {
     public class BasicCalculator
     {
+        private dynamic _result;
+
+        public dynamic Result
+        {
+            get => _result;
+            set => _result = value;
+        }
+
+        Sum Addition = new Sum();
+
+        public dynamic Add(dynamic a, dynamic b)
+        {
+            Result = Addition.Add(a, b);
+            return Result;
+        }
     }
+
+
 }
---
+++ BasicCalculatorTests/BasicCalculatorTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BasicCalculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCalculator.Tests
+{
+    [TestClass()]
+    public class BasicCalculatorTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            BasicCalculator calc = new BasicCalculator();
+            calc.Add(4, 5);
+            Assert.AreEqual(9, calc.Result);
+        }
+    }
+}
