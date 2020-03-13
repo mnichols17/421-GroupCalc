0e100f3899ec31f8a1bebf3d37266c5c7e347509
Henry <hpc3@njit.edu>
2020-03-13 15:45:05 -0400

    Adding BasicCalculator Tests with Add Int test

---
+++ BasicCalculatorTests/BasicCalculatorTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BasicCalculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCalculator.Tests
+{
+    [TestClass()]
+    public class BasicCalculatorTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+
+            BasicCalculator calculator = new BasicCalculator();
+            calculator.Add(5, 4);
+            Assert.AreEqual(9, calculator.Result);
+        }
+    }
+}
