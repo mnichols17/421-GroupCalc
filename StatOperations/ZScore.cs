8cb7a7094eacd877d0f64a483dd6f78620d73a0c
Mike <mike.nichols97@gmail.com>
2020-03-13 22:17:18 -0400

    Z Score method

---
+++ StatOperations/ZScore.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MathOperations;
+
+namespace StatOperations
+{
+    class ZScore
+    {
+        static public void score(int x, int[] nums)
+        {
+            decimal mean = Mean.findMean(nums);
+            decimal SD = StandardDeviation.findSD(nums);
+
+            decimal z = Division.Quotient((x - mean), SD);
+        }
+
+        static public void score(decimal x, decimal[] nums)
+        {
+            decimal mean = Mean.findMean(nums);
+            decimal SD = StandardDeviation.findSD(nums);
+
+            decimal z = Division.Quotient((x - mean), SD);
+        }
+    }
+}
